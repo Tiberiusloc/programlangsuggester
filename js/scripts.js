363f7aad12dd9b5fd55f2ddefa65b5161b48f949
Tiberius Lockett <tlockett680@gmail.com>
2022-08-12 14:11:03 -0700

    WIP adding outcomes to display when branching

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,20 @@ window.addEventListener("load", function() {
   let form = document.getElementById("formOne");
   let resetBtn = document.getElementById("reset");
   let outcome = document.getElementById("outcome");
+  let cSharp = document.getElementById("cSharp");
+  let java = document.getElementById("java");
+  let ruby = document.getElementById("ruby");
    
 form.addEventListener("submit", function(event) {
   
   
   const nameInput = document.getElementById("nameInput").value;
   const questionOne = document.getElementById("questionOne").value;
-  const questionTwo = document.getElementById("questionTwo").value;
+  const questionTwo = parseInt(document.getElementById("questionTwo").value);
   const questionThree = document.getElementById("questionThree").value; 
   const questionFour = document.getElementById("questionFour").value;
-  const questionFive = document.getElementById("questionFive").value;
-  
+  const questionFive = parseInt(document.getElementById("questionFive").value);
+
   document.querySelector("span#name1a").innerText = nameInput;
   document.querySelector("span#question1a").innerText = questionOne;
   document.querySelector("span#question2a").innerText = questionTwo;
@@ -25,6 +28,15 @@ form.addEventListener("submit", function(event) {
   outcome.removeAttribute("class");
   
   event.preventDefault();
+
+  if (questionTwo >= 7 && questionFive >= 3 ) {
+    cSharp.removeAttribute("class");
+  } else if (questionTwo >= 4 || questionFive >= 1) {
+    java.removeAttribute("class");
+  } else if (questionTwo >= 1 || questionFive >= 1) {
+    ruby.removeAttribute("class");
+  }
+
   });
 
   form.addEventListener("submit", function() {
