04dc04a7ae0e7aa3656e7ae2cafd70f05d3ee209
Tiberius Lockett <tlockett680@gmail.com>
2022-08-12 12:35:36 -0700

    Add reset button and event listener for load func etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 //Buisness Logic
 
-window.onload = function() {
-
-let form = document.getElementById("formOne")
-form.onsubmit = function(event) {
+window.addEventListener("load", function() {
+  let form = document.getElementById("formOne");
+  let resetBtn = document.getElementById("reset");
+  let outcome = document.getElementById("outcome");
+   
+form.addEventListener("submit", function(event) {
   
   
   const nameInput = document.getElementById("nameInput").value;
@@ -19,8 +21,23 @@ form.onsubmit = function(event) {
   document.querySelector("span#question3a").innerText = questionThree;
   document.querySelector("span#question4a").innerText = questionFour;
   document.querySelector("span#question5a").innerText = questionFive;
+
+  outcome.removeAttribute("class");
   
-  document.querySelector("div#outcome").removeAttribute("class");
-  event.preventDefault(event);
-  };
-};
+  event.preventDefault();
+  });
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    outcome.setAttribute("class", "hidden");
+    document.getElementById("nameInput").value = null;
+    document.getElementById("questionOne").value = null;
+    document.getElementById("questionTwo").value = null;
+    document.getElementById("questionThree").value = null;
+    document.getElementById("questionFour").value = null;
+    document.getElementById("questionFive").value = null;
+  });
+});
