5d3354c4e7a259a5a870d4f44227ac70b6a8f092
Tiberius Lockett <tlockett680@gmail.com>
2022-08-12 10:19:00 -0700

    WIP for adding inputs

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,15 @@ form.onsubmit = function(event) {
   const nameInput = document.getElementById("nameInput").value;
   const questionOne = document.getElementById("questionOne").value;
   const questionTwo = document.getElementById("questionTwo").Value;
-  console.log("questionTree")
   const questionThree = document.getElementById("questionThree").Value;
   const questionFour = document.getElementById("questionFour").Value;
   const questionFive = document.getElementById("questionFive").Value;
 
   document.querySelector("span#name1a").innerText = nameInput;
-  document.querySelector("span#questionOne1a").innerText = questionOne;
-  document.querySelector("span#questionTwo2a").innerText = questionTwo;
-  document.querySelector("span#questionThree3a").innerText = questionThree;
-  document.querySelector("span#questionFour4a").innerText = questionFour;
-  document.querySelector("span#questionFive5a").innerText = questionFive;
+  document.querySelector("span#question1a").innerText = questionOne;
+  document.querySelector("span#question2a").innerText = questionTwo;
+  document.querySelector("span#question3a").innerText = questionThree;
+  document.querySelector("span#question4a").innerText = questionFour;
+  document.querySelector("span#question5a").innerText = questionFive;
 }
 }
