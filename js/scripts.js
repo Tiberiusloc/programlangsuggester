d39caf125960f0ff89e929ed030076f8495c1f5d
Tiberius Lockett <tlockett680@gmail.com>
2022-08-12 12:01:07 -0700

    Add hidden class and p for checking answers WIP branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 //Buisness Logic
 
-
 window.onload = function() {
 
 let form = document.getElementById("formOne")
 form.onsubmit = function(event) {
-
-  event.preventDefault();
-
+  
+  
   const nameInput = document.getElementById("nameInput").value;
   const questionOne = document.getElementById("questionOne").value;
-  const questionTwo = document.querySelector("input[name='questionTwo']:checked").value;
+  const questionTwo = document.getElementById("questionTwo").value;
   const questionThree = document.getElementById("questionThree").value; 
   const questionFour = document.getElementById("questionFour").value;
   const questionFive = document.getElementById("questionFive").value;
-
+  
   document.querySelector("span#name1a").innerText = nameInput;
   document.querySelector("span#question1a").innerText = questionOne;
   document.querySelector("span#question2a").innerText = questionTwo;
   document.querySelector("span#question3a").innerText = questionThree;
   document.querySelector("span#question4a").innerText = questionFour;
   document.querySelector("span#question5a").innerText = questionFive;
-}
-}
+  
+  document.querySelector("div#outcome").removeAttribute("class");
+  event.preventDefault(event);
+  };
+};
